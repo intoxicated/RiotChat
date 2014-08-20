ba475f164520413ebc2ff66ceae3a33edf21a575
Wooyoung Chung <yakle1218@gmail.com>
2014-08-20 00:02:14 -0700

    create chatserver test skeleton

---
+++ test/chatserver_test.py
@@ -0,0 +1,67 @@
+from unittest import TestCase
+
+class TestBasicChatServer(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_connect_server(self):
+        pass
+
+    def test_disconnect_server(self):
+        pass
+
+class BasicChatServerTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+class SimpleLogTest(BasicChatServerTest):
+    def test_login(self):
+        pass
+
+    def test_logout(self):
+        pass
+     
+class TestChatServerFeatures(BasicChatServerTest):
+    def test_send_msg(self):
+        pass
+
+    def test_receive_msg(self):
+        pass
+
+    def test_add_grouplist(self):
+        pass
+
+    def test_get_grouplist(self):
+        pass
+
+    def test_remove_grouplist(self):
+        pass
+
+    def test_get_blocklist(self):
+        pass
+
+    def test_get_friendlist(self):
+        pass
+
+    def test_get_recentplayed(self):
+        pass
+
+    def test_add_friend(self):
+        pass
+
+    def test_remove_friend(Self):
+        pass
+
+    def test_add_block(self):
+        pass
+
+    def test_remove_block(self):
+        pass
+
+    
