99ee067b9e72c06470cf659d9e5ae78489d84aa0
Wooyoung Chung <yakle1218@gmail.com>
2014-08-19 23:52:27 -0700

    create models

---
+++ models/stat.py
@@ -0,0 +1,13 @@
+""" 
+Stat represents various stat of user 
+"""
+
+class Stat (object):
+    """ 
+        
+        TBD
+
+    """
+    def __init__(self):
+        pass
+    
---
+++ models/user.py
@@ -0,0 +1,23 @@
+""" 
+User represents a user of lol player
+"""
+
+class User(object):
+    """
+
+
+
+
+
+    """
+    def __init__(self):
+        pass
+
+
+class Friend(User):
+
+    def __init__(self):
+        super(Friend, self).__init__()
+        self.groupid = 0
+        self.note = ''
+        
