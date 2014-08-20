6d55ec45562fdeaf954acd96ebcb5f2ad1043bcb
Wooyoung Chung <yakle1218@gmail.com>
2014-08-19 23:53:07 -0700

    create utility modules

---
+++ utils/misc.py
@@ -0,0 +1,21 @@
+"""
+
+miscellous 
+
+"""
+
+class Division():
+    NONE, I, II, III, IV, V = range(6)
+
+
+class Tier():
+    UNRANKED, BRONZE, SILVER, GOLD,\
+    PLATINUM, DIAMOND, CHALLENGER = range(7)
+    
+class GameStatus():
+    pass
+
+class Queue():
+    pass
+
+
