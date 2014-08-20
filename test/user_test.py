4205abc5a9efc71aeaf67acea3e2f24f542725f8
Wooyoung Chung <yakle1218@gmail.com>
2014-08-20 00:28:45 -0700

    add more test cases

--- test/user_test.py
+++ test/user_test.py
@@ -1,14 +1,10 @@
 import unittest 
 from datetime import datetime
 from models.user import User
-from models.riot_exception import RiotInvalidRangeError
+from utils.misc import Division, Tier, GameStatus
+from models.riot_exception import *
 
 class UserTest(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
 
     def test_create_user(self):
         time = datetime.now()
@@ -18,9 +14,7 @@ class UserTest(unittest.TestCase):
         user.revisionDate = time
         user.level = 30
         user.name = 'intoxicated'
-        user.tier = 'BRONZE'
-        user.division = 'I'
-
+        
         self.assertEquals(user.id, 38413)
         self.assertEquals(user.profIcon, 3134)
         self.assertEquals(user.revisionDate, time)
@@ -30,14 +24,24 @@ class UserTest(unittest.TestCase):
     def test_constructor(self):
         time = datetime.now()
         user = User(38413, 'intoxicated', 3134, 30, time)
-        user.status = 'away'
         
-        self.assertEquals(user.status, 'away')
         self.assertEquals(user.id, 38413)
         self.assertEquals(user.profIcon, 3134)
         self.assertEquals(user.revisionDate, time)
         self.assertEquals(user.level, 30)
         self.assertEquals(user.name, 'intoxicated')
+
+    def test_user_status(self):
+        time = datetime.now()
+        user = User(38413, 'intoxicated', 3134, 30, time)
+
+        user.tier = Tier.BRONZE
+        user.division = Division.I
+        user.gameStatus = GameStatus()
+
+        self.assertEquals(user.tier, 1)
+        self.assertEquals(user.division, 1)
+        self.assertEquals(user.gameStatus, 3)
 
     def test_invalid_args(self):
         user = User()
@@ -51,9 +55,9 @@ class UserTest(unittest.TestCase):
         def setLevelHigh():
             user.level = 31
 
-        self.assertRaises(RiotInvalidRangeError, setLevelHigh)
-        self.assertRaises(RiotInvalidRangeError, setLevelLow)
-        self.assertRaises(RiotInvalidRangeError, setStatus)
+        self.assertRaises(RiotRangeError, setLevelHigh)
+        self.assertRaises(RiotRangeError, setLevelLow)
+        self.assertRaises(RiotRangeError, setStatus)
         
     def test_all_other(self):
         pass
