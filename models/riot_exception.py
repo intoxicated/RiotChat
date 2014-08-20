e72edc850e870582d6116f067e9af7fc8b270a2e
Wooyoung Chung <yakle1218@gmail.com>
2014-08-19 23:52:37 -0700

    create exception class

---
+++ models/riot_exception.py
@@ -0,0 +1,17 @@
+"""
+
+
+"""
+
+class RiotException(Exception):
+    def __init__(self, msg):
+        super(RiotException, self).__init__(msg)
+        self.msg = msg
+
+class RiotInvalidRangeError(RiotException):
+    pass
+
+class RiotNameDoesNotExists(RiotException):
+    pass 
+
+
