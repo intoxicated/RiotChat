6395eb11ec1ed7c1f94b55a9cc71b2b345ac7c3c
Wooyoung Chung <yakle1218@gmail.com>
2014-08-21 12:44:13 -0700

    skeleton for RiotChat
    it should handling user inputs, implement command to execute, etc

--- RiotChat.py
+++ RiotChat.py
@@ -1,1 +1,23 @@
+import sys
 
+from core.riotxmpp_client import RiotXMPPClient
+
+class RiotChat(object):
+    pass
+
+    #help
+    #cmd analyze
+
+if __name__ == "__main__":
+    usr = sys.argv[0]
+    pw = sys.argv[1]
+    region = sys.argv[2]
+
+    client = RiotXMPPClient()
+    client.connect()
+
+    while True:
+        cmd = raw_input("RiotChat > ")
+        #analyze command and do something
+        if cmd == "send":
+            client.send(id, msg)
