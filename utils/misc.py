2497e02fb94b0f97fc4ba038cc669380f5ab1ce0
Wooyoung Chung <yakle1218@gmail.com>
2014-08-22 22:38:59 -0700

    added size parameter for max queue

--- utils/misc.py
+++ utils/misc.py
@@ -5,18 +5,35 @@ helper classes
 """
 
 class MessageBuffer(object):
-    def __init__(self):
+    def __init__(self, maxSize):
         self.buf = []
+        self.i = -1
 
     def push(self, msg_from, msg, time):
+        if len(self.buf) > maxSize:
+            self.buf.pop(0)
+
         entry = (msg_from, msg, time)
         self.buf.append(entry)
 
     def pop():
-        if len(self.buf) != 0:
+        if len(self.buf) > 0:
             return self.buf.pop(0)
         else:
             return None
+
+    def __getitem__(self, sliced):
+        return self.buf[sliced]
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.i < len(self.buf)-1:
+            self.i += 1
+            return self.buf[self.i]
+        else:
+            raise StopIteration
 
 """
 
