c202427da7d1d86a1d21b0c5ab3ff18c56fa87d9
Wooyoung Chung <yakle1218@gmail.com>
2014-08-25 15:49:42 -0700

    remove unnecessary enums

--- utils/misc.py
+++ utils/misc.py
@@ -37,35 +37,17 @@ class MessageBuffer(object):
 
 """
 
-Misc enums
-
-"""
-
-class Division():
-    NONE, I, II, III, IV, V = range(6)
-
-
-class Tier():
-    UNRANKED, BRONZE, SILVER, GOLD,\
-    PLATINUM, DIAMOND, CHALLENGER = range(7)
-    
-class GameStatus():
-    pass
-
-class Queue():
-    pass
-
-"""
-
 miscellous methods
 
 """
 
-def jid2summoner(jId):
+def jid2summoner(rosterobj, jid, online):
     pass
-
+    
 def summoner2jid(summonerId):
     pass
+
+
 
 
 
