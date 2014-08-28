139456f872f5d017ebd21edab9b1c32deed301f4
Wooyoung Chung <yakle1218@gmail.com>
2014-08-27 22:24:36 -0700

    add defaultStatus and implemented User
    Friend now inherit from User

--- models/user.py
+++ models/user.py
@@ -5,6 +5,26 @@ User represents a user of lol player
 
 from models.riot_exception import *
 
+defaultStatus = { 
+    'level': 100,
+    'profileIcon': 673,
+    'wins': 0,
+    'leaves': 0,
+    'odinWins': 0,
+    'odinLeaves': 0,
+    'queueType' : '/',
+    'rankedLosses': 0,
+    'rankedRating': 0,
+    'tier': 'UNRANKED',
+    'rankedLeagueName': 'Singed&amp;apos;s Butchers',
+    'rankedLeagueDivision': 'I',
+    'rankedLeagueTier': 'DIAMOND',
+    'rankedLeageuQueue': 'RANKED_SOLO_5X5',
+    'rankedWins': 0,
+    'gameStatus': 'outOfGame',
+    'statusMsg': 'PyRiotChat Client',
+}
+
 class Status(object):
     def __init__(self, args=None):
         #self._level = kwargs['level']
@@ -16,26 +36,53 @@ class Status(object):
 class User(object):
     """
 
-
-
-
-
     """
-    def __init__(self):
-        pass
-
-
-class Friend(User):
-
-    def __init__(self, jid, name, substype, grpName):
-        super(Friend, self).__init__()
-        self._groupName = 'Default' if (grpName == '**Default') else grpName
+    def __init__(self, jid, name, res, status=defaultStatus):
         self._jid = jid
         self._name = name
-        self._substype = substype
-        self._status = {} # Status()
-        self._online = False 
+        self._resource = res
+        self._status = status # Status()
     
+    @property
+    def jid(self):
+        return self._jid
+    
+    @jid.setter
+    def jid(self, jid):
+        self._jid = jid
+
+    @property 
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def resource(self):
+        return self._resource
+
+    @resource.setter
+    def resource(self, newresource):
+        self._resource = newresource
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        self._status = status
+    
+    def form_status(self):
+        formStatus = ""
+        for key, value in self._status.items():
+            if value != None:
+                formStatus += "<" + str(key) + ">" +\
+                    str(value) + "</" + str(key) + ">"
+        return formStatus
+
     def get_status(self):
         formatStr = ""
         
@@ -43,6 +90,14 @@ class Friend(User):
             formatStr += "{:<10} {:<10}".format(k,v)
         return formatStr
 
+class Friend(User):
+
+    def __init__(self, jid, res, name, substype, grpName, status=None):
+        super(Friend, self).__init__(jid, name, res, status)
+        self._groupName = 'Default' if (grpName == '**Default') else grpName
+        self._substype = substype
+        self._online = False 
+        
     @property
     def groupName(self):
         return self._groupName
@@ -59,21 +114,6 @@ class Friend(User):
     def online(self, online):
         self._isOnline = online
 
-    @property
-    def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, status):
-        self._status = status
-
-    @property
-    def jid(self):
-        return self._jid
-
-    @property 
-    def name(self):
-        return self._name
 
 class RosterManager(object):
     def __init__(self):
@@ -103,7 +143,7 @@ class RosterManager(object):
         else:
             return False
 
-    def updateStatus(self, jid, status, online=False):
+    def updateStatus(self, jid, resource, status, online=False):
         fentry = "" #dummy
         if online:
             #if originally offline
@@ -116,6 +156,7 @@ class RosterManager(object):
                 fentry = self._onlineGrp[jid]
             #update status
             fentry.status = status
+            fentry.resource = resource
             #insert into online
             self._onlineGrp[jid] = fentry
 
@@ -135,12 +176,25 @@ class RosterManager(object):
             del self._offlineGrp[jid]
         else:
             raise Exception
+    
+    def summoner2jid(self, summoner):
+        for k,v in self._onlineGrp.items():
+            if v.name == summoner:
+                return k
+        
+        for k,v in self._offlineGrp.items():
+            if v.name == summoner:
+                return k
+    
+        return None
 
-    def jid2summoner(self, jid, online):
-        if online == False:
+    def jid2summoner(self, jid):
+        if self._onlineGrp.get(jid) != None:
+            return self._onlineGrp[jid].name
+        elif self._offlineGrp.get(jid) != None:
             return self._offlineGrp[jid].name
         else:
-            return self._offlineGrp[jid].name
+            pass # exception or use riot api to find name 
 
     def get_friend(self, jid):
         if self._onlineGrp.get(jid) != None :
