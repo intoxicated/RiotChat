6202d0809ba66a84c04770f602a8e9bc3b831772
Wooyoung Chung <yakle1218@gmail.com>
2014-08-27 22:51:54 -0700

    Added client implementation

---
+++ riotxmpp_client.py
@@ -0,0 +1,187 @@
+from riotxmpp import RiotXMPP
+from models.serverlist import *
+from models.cmds import *
+from models.user import User, Friend, RosterManager
+import sys
+import os
+
+"""
+    extends RiotXMPP to provide full features 
+
+"""
+
+class RiotXMPPClient(RiotXMPP):
+    def __init__(self, usrname, pw, region="NA", verbose=False):
+        super(RiotXMPPClient, self).__init__(usrname, pw, region, verbose)
+        
+        #add additional handler
+        self.add_event_handler("got_online", self.got_online)
+        self.add_event_handler("got_offline", self.got_offline)
+        self.add_event_handler("roster_update", self.roster_update)
+        self.add_event_handler("on_message", self.on_message)
+        self.add_event_handler("add_event", self.add_event)
+        self.add_event_handler("remove_event", self.remove_event)
+        self.add_event_handler("connected", self.connected)
+        self.add_event_handler("disconnected", self.disconnected)
+        self.add_event_handler("grp_invitation", self.grp_invitation)
+        #
+    
+    def help(self):
+        formatStr = ""
+        for cmd, desc in cmdlst.items():
+            formatStr += "{:>9} {:<15} {:<20}\n".format(cmd, desc[0], desc[1])
+            if cmd == 'display':
+                for dk, ddesc in desc[2].items():
+                    formatStr += "{:>9} {:<5} {:<9} {:<40}\n".\
+                        format("","@type",dk, ddesc)
+
+        print formatStr
+
+    def start(self):
+        self.connect()
+
+    def stop(self):
+        self.disconnect()
+    
+    def quit(self):
+        if self.verbose:
+            print "Terminating the program.."
+        exit()
+
+    def on_message(self, msgfrom, msg, stamp):
+        print "{:<} {:<} {}".format(msgfrom, stamp, msg)
+
+    def got_online(self, summoner_id):
+        pass
+
+    def got_offline(self, summoner_id):
+        pass
+
+    def roster_update(self, roster_update):
+        pass
+
+    def add_event(self, summoner_id):
+        pass
+
+    def remove_event(self, presence):
+        pass
+
+    def connected(self):
+        pass
+
+    def disconnected(self):
+        pass
+
+    def grp_invitation(self):
+        #join room
+        pass
+
+    #command wappers 
+    def send(self, to, msg):
+        if self.verbose:
+            print "SEND %s %s" % (to, msg)
+
+        self.send_message(to, msg, "chat")
+
+    def add(self, summoner_id):
+        if "@" not in summoner_id: #summoner name
+            pass
+        else:
+            self.add_friend(summoner_id)
+
+    def remove(self, summoner_id):
+        if "@" not in summoner_id: #summoner name 
+            pass
+        else:
+            self.remove_friend(summoner_id)
+
+    def clear(self):
+        os.system('clear')
+
+    def display(self, args):
+        print "ARGS TYPE: %s ARGS: %s" % \
+                (type(args), args[0])
+        if args[0] == 'all':
+            self.display_all()
+        elif args[0] == 'online':
+            self.display_online()
+        elif args[0] == 'history' and args[1] != None:
+            self.display_history(args[1])
+        elif args[0] == 'status' and args[1] != None:
+            self.display_status(args[1])
+        else:
+            print args
+            print "invalid arguments for display"
+
+    def display_all(self):
+        #display all friends 
+        flst = self.roster_manager.get_all()
+        resultStr = ""
+        for k,v in flst.items():
+            resultStr += v.name + "\n"  
+        
+        print resultStr
+
+    def display_online(self):
+        #display online friends
+        flst = self.roster_manager.onlineGrp
+        resultStr = ""
+        for k,v in flst.items():
+            resultStr += v.name + "\n"
+        
+        print resultStr
+
+    def display_history(self, jid):
+        #display past # of messages with summoner, or grp
+        print "DISPLAY HISTORY WITH %s" % jid
+        pass
+
+    def display_status(self, summoner):
+        fentry = self.roster_manager.get(summoner)
+        print fentry.get_status()
+
+def parse_cmd(cmds):
+    cmdlst = cmds.split(" ")
+
+    if len(cmdlst) == 1:
+        return cmdlst[0], None
+    else:
+        return cmdlst[0], cmdlst[1:]
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    pw = sys.argv[2]
+    region = sys.argv[3]
+    print usr,pw, region
+
+    client = RiotXMPPClient(usr,pw,region, verbose=True)
+    #client.start()
+
+    while True:
+        #handle command
+        cmds = raw_input("RiotChat > ")
+        if cmds == "":
+            continue
+        #parse cmds 
+        cmd, args = parse_cmd(cmds)
+        print "func: %s args: %s" % (cmd, args)
+        if cmd == "send":
+            client.send(args[0], "".join(args[1:]))
+        elif cmd == "start":
+            client.start()
+        elif cmd == "stop":
+            client.stop()
+        elif cmd == "clear":
+            client.clear()
+        elif cmd == "display":
+            client.display(args)
+        elif cmd == "add":
+            pass
+        elif cmd == "remove":
+            pass
+        elif cmd == "quit":
+            client.quit() 
+        elif cmd == "invite":
+            pass
+        else:
+            print "Cannot recognize command"
